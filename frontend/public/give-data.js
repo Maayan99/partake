900bc7f14b1dc064f526fb6c111b12fc67e972af
Maayan <maayansonnenfeld@gmail.com>
2023-04-24 15:21:54 +0300

    give data

--- frontend/public/give-data.js
+++ frontend/public/give-data.js
@@ -31,6 +31,36 @@ const template = {
 
 const giveData = [
     {
+        id: 17,
+        title: 'Donate goods  for families in need.',
+        coverImage: '',
+        type: 'Goods Donation', //Either Field Volunteering, Online Voluneering, Goods Donation, or Fundraising
+        author: 'מיד ליעד',
+        about: 'Our charity is collecting donations of electricity products, furniture, and baby products to distribute to those who are struggling.', //About the NGO or the Give
+        description: "If you want to make a difference in someone's life, simply call us and arrange a date for us to pick up your donations. We will ensure that your items go to a family that truly needs them, providing them with essential household items that they might not otherwise be able to afford. By donating to our charity, you can make a meaningful impact on the lives of those who are less fortunate, while also promoting sustainability by reducing waste. Contact us today to schedule your donation and help us make a positive difference in the world.",
+        skillBased: false,
+        skills: ['Coding', 'Teaching'],
+        location: 'We collect goods from Gush-Dan area.',
+        contactDetails:
+            {
+                name: '',
+                phone: '+972-03-6198214',
+                email: 'meyad2@gmail.com',
+                website: 'https://layaad.org.il/',
+            },
+        reward: '',
+        requirements: ['We deliver only furniture, electricity products and baby products', 'furniture and baby products must be in good condition', 'All electricity products must be working.'],
+        impactType: 'Social',
+        impactReward: 'Bag- items donated',
+        impact: ['People in need', 'Children and Youth', 'Strengthen Local community', 'waste reduction', 'Re-use',],
+        timeDetails:
+            {
+                endDate: '',
+                frequency: '',
+                duration: '',
+            },
+    },
+    {
         id: 18,
         title: '',
         coverImage: '',
