b9cb1c2765ebf74bb2f7bdca84dd543be54e488b
Maayan <maayansonnenfeld@gmail.com>
2023-04-27 11:54:42 +0300

    fixed the nav buttons

--- frontend/src/components/common/unselectedButton.js
+++ frontend/src/components/common/unselectedButton.js
@@ -4,7 +4,7 @@ const UnselectedButton = (props) => {
     const { children, className, ...otherProps } = props;
 
     return (
-        <button className={`${className} bg-white text-black py-4 px-8 rounded-full cursor-pointer
+        <button className={`${className} bg-white text-black py-2 px-6 rounded-full cursor-pointer
         transition-all duration-300 hover:bg-blue text-3xl mx-2`} {...otherProps}>
             {children}
         </button>
