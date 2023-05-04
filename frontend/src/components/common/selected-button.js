b901477509b55b131dfb9946230e6e67ed62def2
Maayan <maayansonnenfeld@gmail.com>
2023-05-04 18:54:09 +0300

    small fiddling with buttons to make the nav buttons have a nice animation

--- frontend/src/components/common/selected-button.js
+++ frontend/src/components/common/selected-button.js
@@ -5,8 +5,10 @@ const SelectedButton = (props) => {
     const { children, className, ...otherProps } = props;
 
     return (
-        <button className={`${className} bg-black text-white py-2 px-6 rounded-full cursor-pointer
-        transition-all duration-300 hover:scale-110 text-3xl mx-2`} {...otherProps}>
+        <button className={`${className} bg-black text-white py-2 px-6 rounded-full cursor-pointer text-3xl mx-2 overflow-hidden
+       relative isolate before:content-[''] before:h-[20%] before:aspect-square before:bg-blue 
+       before:transition-transform before:duration-700 before:opacity-0 hover:before:opacity-100 
+         before:absolute before:inset-0 before:m-auto before:rounded-full before:z-[-1] hover:before:scale-[15]`} {...otherProps}>
             {children}
         </button>
     );
--- frontend/src/components/common/unselected-button.js
+++ frontend/src/components/common/unselected-button.js
@@ -4,8 +4,12 @@ const UnselectedButton = (props) => {
     const { children, className, ...otherProps } = props;
 
     return (
-        <button className={`${className} bg-transparent text-black py-2 px-6 rounded-full cursor-pointer
-        transition-all duration-300 hover:scale-110 text-3xl mx-2`} {...otherProps}>
+        <button className={`${className} text-black py-2 px-6 rounded-full cursor-pointer
+        text-3xl mx-2 overflow-hidden
+       relative isolate before:content-[''] before:h-[20%] before:aspect-square before:bg-light-gray 
+       before:transition-transform before:duration-700 before:opacity-0 hover:before:opacity-100 
+         before:absolute before:inset-0 before:m-auto before:rounded-full before:z-[-1] hover:before:scale-[15]
+        `} {...otherProps}>
             {children}
         </button>
     );
