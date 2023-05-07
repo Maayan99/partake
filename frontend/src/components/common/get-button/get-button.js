114d39b01cfbf5b05acf8bdc397d098eb2edc25b
Maayan <maayansonnenfeld@gmail.com>
2023-05-07 22:04:37 +0300

    Get nav buttons and nav row in get area

---
+++ frontend/src/components/common/get-button/get-button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GetButton = (props) => {
+    const { children, className, selected, ...otherProps} = props;
+
+    return (
+        <button className={`${className} ${selected ? 'bg-black text-white' : 'bg-white text-black'} py-2 px-6 
+        border-black border-2 rounded-2xl cursor-pointer text-xl mx-2 
+        `} {...otherProps}>
+            {children}
+        </button>
+    );
+};
+
+export default GetButton;
+
--- frontend/src/pages/get/index.js
+++ frontend/src/pages/get/index.js
@@ -1,9 +1,39 @@
 import getData from "../../../public/get-data";
 import SmallGetCard from "@components/components/cards/small-get-card/small-get-card";
+import {useState} from "react";
+import GetButton from "@components/components/common/get-button/get-button";
 
 export default function Get() {
+    let filtered = getData;
+
+    const [selectedCategory, setSelectedCategory] = useState('1');
+
+    const categories = [
+        {
+            id: '1',
+            text: 'Social',
+        },
+        {
+            id: '2',
+            text: 'Sustainability',
+        },
+        {
+            id: '3',
+            text: 'Philanthropy',
+        },
+        {
+            id: '4',
+            text: 'Well-being',
+        },
+        {
+            id: '5',
+            text: 'Diversity & Inclusion',
+        },
+    ];
+
+
     return (
-        <div className="p-20">
+        <div className="p-20 space-y-10">
             <div className="grid grid-cols-4 gap-4 w-full">
                 <img src={`/assets/PNG/donate-mock-image.webp`} className="col-span-2 row-span-3 object-cover w-full h-full"/>
                 <div className="col-span-2 row-span-2 h-full w-full bg-important-blue flex flex-col items-center justify-center p-10 text-center">
@@ -12,6 +42,12 @@ export default function Get() {
                 </div>
                 <img src={`/assets/PNG/blue-monday-mock.jpeg`} className="object-cover w-full h-full"/>
                 <img src={`/assets/PNG/gift-card-mock.jpeg`} className="object-cover w-full h-full"/>
+            </div>
+
+            <div className="flex space-x-6">
+                {categories.map(category => <GetButton key={category.id} onClick={() => setSelectedCategory(category.id)}
+                                                       selected={selectedCategory === category.id}
+                >{category.text}</GetButton>)}
             </div>
         </div>
     )
