0d84f901743f8f06e5108459f6bbbbcc423d496d
Maayan <maayansonnenfeld@gmail.com>
2023-05-09 15:26:34 +0300

    filter bar

--- frontend/src/components/filter-bar/filter-bar.js
+++ frontend/src/components/filter-bar/filter-bar.js
@@ -7,12 +7,13 @@ export default function FilterBar({search, setSearch, filterersArray}) {
 
     return (
         <div className="flex ml-36 h-24 items-center space-x-8 mb-10 ">
-            <div className="flex h-10 focus-within:shadow border-r border-1 border-gray-500">
-                <input placeholder="Search" className="w-72 pl-1 border border-1 border-gray-500 bg-light-gray
+            <div className="flex h-14 focus-within:shadow border-r border-1 border-gray-500">
+                <input placeholder="Search" className="italic w-72 font-thin px-4 py-2 text-xl
+                 border border-1 border-gray-500 bg-light-gray
                 rounded-none border-r-0 focus:outline-none" onChange={handleSearchChange} value={search}/>
                 {filterersArray.map(filterer =>
                     <select key={filterer.id} onChange={filterer.handleFunction} value={filterer.value}
-                            className="w-52 border border-1 border-gray-500 bg-light-gray
+                            className="w-52 px-4 py-2 border border-1 border-gray-500 bg-light-gray text-xl
                 rounded-none border-r-0 focus:outline-none">
                         <option value="">{filterer.placeholder}</option>
                         {filterer.options.map(option =>
@@ -23,7 +24,7 @@ export default function FilterBar({search, setSearch, filterersArray}) {
                     </select>)}
             </div>
             <a href="/make">
-                <div className="w-24 h-24 border border-4 border-black rounded-full
+                <div className="w-24 h-24 border border-2 border-black rounded-full
                 transform-all duration-150 hover:scale-110 flex justify-center items-center">
                     <Icon name="plus" className="h-8 w-8"/>
                 </div>
