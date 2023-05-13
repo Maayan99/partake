8430a56a196dcdbe37ab26bbac62ebe67b6efdda
Ofer Getz <ofergetz@gmail.com>
2023-05-13 22:24:38 +0300

    participants photos

--- frontend/src/components/participants-row/participants-row.js
+++ frontend/src/components/participants-row/participants-row.js
@@ -4,15 +4,15 @@ import React from "react";
 export default function ParticipantsRow({participants}) {
     return (
         <div className="w-32 grid grid-cols-7">
-            <img src="assets/PNG/profile-images/profile_image_2.jpeg" alt="Profile" className="border border-white rounded-full
+            <img src="assets/PNG/profile-images/avatar-5.png" alt="Profile" className="border border-white rounded-full
             min-w-[32px] h-8 object-cover hover:z-10"/>
-            <img src="assets/PNG/profile-images/profile_image_3.jpeg" alt="Profile" className="border border-white rounded-full
+            <img src="assets/PNG/profile-images/avatar-8.png" alt="Profile" className="border border-white rounded-full
              min-w-[32px] h-8 object-cover hover:z-10"/>
-            <img src="assets/PNG/profile-images/profile_image_4.jpeg" alt="Profile" className="border border-white rounded-full
+            <img src="assets/PNG/profile-images/avatar-11.png" alt="Profile" className="border border-white rounded-full
              min-w-[32px] h-8 object-cover hover:z-10"/>
-            <img src="assets/PNG/profile-images/profile_image_5.jpeg" alt="Profile" className="border border-white rounded-full
+            <img src="assets/PNG/profile-images/avatar-19.png" alt="Profile" className="border border-white rounded-full
              min-w-[32px] h-8 object-cover hover:z-10"/>
-            <img src="assets/PNG/profile-images/profile_image_6.jpeg" alt="Profile" className="border border-white rounded-full
+            <img src="assets/PNG/profile-images/avatar-1.png" alt="Profile" className="border border-white rounded-full
              min-w-[32px] h-8 object-cover hover:z-10"/>
             <div className="cursor-default flex justify-center items-center bg-blue min-w-[32px] h-8 rounded-full border-white border">
                 <p className="text-white text-xs">{participants - 5}+</p>
