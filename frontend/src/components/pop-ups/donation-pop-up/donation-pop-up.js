c08d97dac760ab86a835c5872505b5a25a51099d
Maayan <maayansonnenfeld@gmail.com>
2023-05-21 20:46:54 +0300

    Popup work as well as impact template

--- frontend/src/components/pop-ups/donation-pop-up/donation-pop-up.js
+++ frontend/src/components/pop-ups/donation-pop-up/donation-pop-up.js
@@ -1,11 +1,84 @@
 import PopUp from "@components/components/pop-ups/basic-pop-up/pop-up";
 import BlueButton from "@components/components/common/blue-button";
+import {useState} from "react";
+import Dropdown from "@components/components/common/custom-dropdown/custom-dropdown";
+import Icon from "@components/components/common/icon/icon";
+
+const currencyOptions =
+    {
+        options:
+            [
+                {
+                    id: '0',
+                    value: 'nis',
+                    text: 'NIS',
+                },
+                {
+                    id: '1',
+                    value: 'usd',
+                    text: 'USD',
+                },
+                {
+                    id: '2',
+                    value: 'euro',
+                    text: 'EUR',
+                },
+            ]
+    };
+
+const donationOptions =
+    {
+        options:
+            [
+                {
+                    id: '0',
+                    value: 'public',
+                    text: 'Public Donation',
+                },
+                {
+                    id: '1',
+                    value: 'name',
+                    text: 'Name Only',
+                },
+                {
+                    id: '2',
+                    value: 'donation',
+                    text: 'Donation Only',
+                },
+            ],
+    };
+
+const presets = [10, 20, 50, 100];
 
 export default function DonationPopUp({display, setDisplay}) {
+    const [amountToDonate, setAmountToDonate] = useState();
+
+    const [anonOrPublic, setAnonOrPublic] = useState();
+
+    const handlePresetClick = (e) => {
+        setAmountToDonate(e.target.id)
+    }
+
     return (
         <PopUp display={display} setDisplay={setDisplay} title="Donation">
             <div className="p-8 flex flex-col items-center space-y-4">
-                <BlueButton onClick={() => {window.location.href = '/take'}}>Take me to more challenges!</BlueButton>
+                <div className="relative w-full">
+                    <input type="number" className="w-full h-10 bg-light-gray p-2"
+                           value={amountToDonate} onChange={(e) => setAmountToDonate(e.target.value)}>
+                    </input>
+                    <div
+                        className="flex space-x-2 items-center absolute right-0 top-1/2 -translate-y-1/2 h-2/3 bg-white p-2 mr-2">
+                        <h3>ILS</h3>
+                        <Icon name="down-arrow" className="h-3.5 w-3.5"/>
+                    </div>
+                </div>
+
+                <div className="flex justify-between w-full">
+                    {presets.map(preset => <button id={preset.toString()} key={preset} onClick={handlePresetClick}
+                                                   className="bg-white border w-16 h-10 rounded-md text-blue">{preset} ₪</button>)}
+                </div>
+
+                <Dropdown selectOptions={donationOptions} className="w-full"/>
             </div>
         </PopUp>
     )
