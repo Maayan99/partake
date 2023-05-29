1d6f8d7e13a12443f2ff168810e31558d4d752a9
Maayan <maayansonnenfeld@gmail.com>
2023-05-29 23:47:03 +0300

    added padding top to pop up

--- frontend/src/components/pop-ups/basic-pop-up/pop-up.js
+++ frontend/src/components/pop-ups/basic-pop-up/pop-up.js
@@ -7,13 +7,13 @@ export default function PopUp({children, display, title, setDisplay, icon}) {
         <>
             {display &&
                 <div className="w-screen h-screen fixed top-0 left-0 z-50 bg-black/50" aria-modal="true">
-                    <div className="w-[420px] bg-white rounded-lg shadow-lg items-center
+                    <div className="w-[420px] bg-white rounded-lg shadow-lg items-center pt-2
         flex flex-col fixed top-1/2 left-1/2 border border-blue border-b-0 border-l-0 border-r-0 border-t-[20px]
         transform -translate-x-1/2 -translate-y-1/2 z-50">
                         <IconButton className="absolute top-0 left-0" onClick={() => setDisplay(false)}>
                             <CloseIcon/>
                         </IconButton>
-                        {icon && <div className="-translate-y-4 shadow-xl
+                        {icon && <div className="-translate-y-6 shadow-xl
                         bg-white p-4 border-blue border
                          rounded-full">
                             <Icon name={icon} className="h-12"/>
