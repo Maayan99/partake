b1722d84d9a563138c1ba436fe9f3f50889bda84
Maayan <maayansonnenfeld@gmail.com>
2023-05-29 12:53:16 +0300

    And now it's hidden without any plugins :)

--- frontend/src/pages/take/index.js
+++ frontend/src/pages/take/index.js
@@ -59,7 +59,7 @@ const BeforeSearch = () => {
                         <ArrowForwardIosIcon/>
                     </div>
                     {/*pb-24 -mb-24 for overflow reasons*/}
-                    <div className="flex pl-36 pt-20 -mt-20 pb-24 -mb-20 space-x-5 overflow-x-auto scrollbar-none">
+                    <div className="flex pl-36 pt-20 -mt-20 pb-24 -mb-20 space-x-5 overflow-x-auto hide-scrollbar">
                         {categoriesDictionary[category].map(take => <SmallTakeCard key={take.id} take={take}/>)}
                     </div>
                 </div>)}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -55,10 +55,7 @@ module.exports = {
               }
         },
     },
-    plugins: [
-        require('tailwind-scrollbar'),
-        require('tailwind-scrollbar-hide'),
-    ],
+    plugins: [],
 }
 
 
