c5ad1616b3cc9932eedf375adea447430615f6ab
Maayan <maayansonnenfeld@gmail.com>
2023-05-30 13:20:10 +0300

    removed pesky lil partake coins from give cover card

--- frontend/src/components/cards/give-cover-card/give-cover-card.js
+++ frontend/src/components/cards/give-cover-card/give-cover-card.js
@@ -8,11 +8,10 @@ const GiveCoverCard = ({give}) => {
     return (
         <div
             className="w-full min-h-[420px] h-full  rounded-lg text-xs
-            flex flex-col justify-between bg-cover bg-center"
+            flex flex-col justify-end bg-cover bg-center"
             style={{backgroundImage: `
             linear-gradient(to bottom, rgba(2,0,36,0.4) 0%, rgba(0,0,0,0) 38%, rgba(0,0,0,0) 66%, rgba(0,0,0,0.65) 100%),
             url("/assets/PNG/give/${coverImage}")`}}>
-            <TakeIcons duration={duration} impactType={impactType} coins={coins} displayRewardIcon={displayRewardIcon}/>
             <div className="text-white ml-2.5 mb-2.5">
                 <h2 className="font-bold text-2xl leading-7 drop-shadow-md">
                     {title}
