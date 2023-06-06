11928f876385c414f20a58de1d02ab986d303fb0
Maayan <maayansonnenfeld@gmail.com>
2023-06-06 18:58:43 +0300

    Impact graphic finished (kinda...)

--- frontend/public/take-data.js
+++ frontend/public/take-data.js
@@ -275,20 +275,23 @@ const takeData = [
         ],
         impact: {
             type: 'environmental',
+
+            tooltips:
+                [
+                    {
+                        id: '1',
+                        title: 'Digital Footprint',
+                        text: 'Deleting a single email can reduce an average of 0.4 grams of CO2 emissions.',
+                        icon: 'digital-footprint',
+                    }
+                ],
+
             categories: [
                 {
                     icon: 'carbon',
                     boldText: 'CO2 emissions',
                     text: '',
-                    tooltips:
-                        [
-                            {
-                                id: '1',
-                                title: 'Digital Footprint',
-                                text: 'Deleting a single email can reduce an average of 0.4 grams of CO2 emissions.',
-                                icon: 'digital-footprint',
-                            }
-                        ],
+
                 }],
 
         },
@@ -444,34 +447,31 @@ const takeData = [
 
         impact:
             {
+                tooltips:
+                    [
+                        {
+                            title: 'Relaxation',
+                            text: 'Research shows that participants who practiced mindfulness meditation on a daily basis experienced a decrease in stress by 31%. (Chiesa & Serretti, 2009).',
+                            icon: 'relaxation',
+                        },
+                        {
+                            title: 'Mindfulness',
+                            text: 'A study reported that individuals who engaged in regular mindfulness meditation experienced a 20% decrease in symptoms of anxiety and a 15% decrease in symptoms of depression (Hofmann et al., 2010).',
+                            icon: 'lotus',
+                        },
+
+                    ],
                 type: 'wellness',
                 categories: [
                     {
                         icon: 'happy',
                         boldText: 'Well-being',
                         text: '',
-
-
-                        tooltips: [
-                            {
-                                title: 'Relaxation',
-                                text: 'Research shows that participants who practiced mindfulness meditation on a daily basis experienced a decrease in stress by 31%. (Chiesa & Serretti, 2009).',
-                                icon: 'relaxation',
-                            },]
                     },
                     {
                         icon: 'balance',
                         boldText: 'Work-life balance',
                         text: '',
-
-                        tooltips:
-                            [
-                                {
-                                    title:'Mindfulness',
-                                    text: 'A study reported that individuals who engaged in regular mindfulness meditation experienced a 20% decrease in symptoms of anxiety and a 15% decrease in symptoms of depression (Hofmann et al., 2010).',
-                                    icon: 'lotus',
-                                },
-                            ]
                     },
                 ],
 
@@ -633,13 +633,36 @@ const takeData = [
                 text: '',
                 image: 'recycle3.png',
                 user: {
-                    id: 'aiua123hvefwli1u32v',             
+                    id: 'aiua123hvefwli1u32v',
                 },
             },
         ],
 
 
         impact: {
+            tooltips:
+                [
+                    {
+                        id: '1',
+                        title: 'Waste Reduction',
+                        text: 'Instead of being buried in a landfill site, the packages you collect will be recycled into different products.',
+                        icon: 'waste',
+                    },
+
+                    {
+                        id: '2',
+                        title: 'Paper',
+                        text: 'Recycling one ton of paper packaging can save around 26,500 to 75,700 liters of water.',
+                        icon: 'paper'
+                    },
+
+                    {
+                        id: '3',
+                        title: 'Recycle',
+                        text: "A full  Tamir's Orange Bag that you recycle,  can  save around 1.6 kg to 2.4kg of CO2 emissions",
+                        icon: 'recycle'
+                    }
+                ],
             type: 'environmental',
             categories:
                 [
@@ -647,29 +670,6 @@ const takeData = [
                         icon: 'carbon',
                         boldText: 'CO2 emissions',
                         text: '',
-                        tooltips:
-                            [
-                                {
-                                    id: '1',
-                                    title:'Waste Reduction',
-                                    text: 'Instead of being buried in a landfill site, the packages you collect will be recycled into different products.',
-                                    icon: 'waste',
-                                },
-
-                                {
-                                    id: '2',
-                                    title:'Paper',
-                                    text: 'Recycling one ton of paper packaging can save around 26,500 to 75,700 liters of water.',
-                                    icon: 'paper'
-                                },
-
-                                {
-                                    id: '3',
-                                    title:'Recycle',
-                                    text: "A full  Tamir's Orange Bag that you recycle,  can  save around 1.6 kg to 2.4kg of CO2 emissions",
-                                    icon: 'recycle'
-                                }
-                            ],
                     }
                 ],
             boldText: 'CO2 emissions',
--- frontend/src/components/impact-graphic/impact-graphic.js
+++ frontend/src/components/impact-graphic/impact-graphic.js
@@ -4,46 +4,58 @@ import LearnMoreTooltip from "@components/components/pop-ups/learn-more-tooltip/
 const ImpactCause = ({impactCategory}) => {
 
     return (
-        <div className="flex flex-col items-center w-max ">
-            <Icon name={impactCategory.icon} className={`min-w-[40px] max-w-[40px] aspect-square mb-2`}/>
-            <div>
-                {impactCategory.boldText &&
-                    <h1 className="font-bold text-lg text-center leading-tight">{impactCategory.boldText}</h1>}
-                {impactCategory.text && <p className="text-center">{impactCategory.text}</p>}
-            </div>
-        </div>
+        <>
+            {impactCategory.dontShow ? <div></div> :
+                <div className="flex flex-col items-center w-full min-w-[85px]">
+                    <Icon name={impactCategory.icon} className={`w-10 aspect-square mb-2`}/>
+                    <div>
+                        {impactCategory.boldText &&
+                            <h1 className="font-bold text-[0.9rem] text-center leading-tight">{impactCategory.boldText}</h1>}
+                        {impactCategory.text && <p className="text-center">{impactCategory.text}</p>}
+                    </div>
+                </div>}
+        </>
     )
 }
 
 export default function ImpactGraphic({impact, showIconsBelow, dontShowTotal}) {
     const {categories, type, total} = impact;
 
+    let filledUpCategories = [];
+
+    for (let i = 0; i < 3; i++) {
+        if (i < categories.length) {
+            filledUpCategories[i] = categories[i];
+        } else {
+            filledUpCategories[i] = {dontShow: true};
+        }
+    }
+
+    const tooltips = impact.tooltips || categories.map(category => (category.tooltips &&
+        category.tooltips.map(tooltip => tooltip)));
 
     return (
-        <div className="grid grid-cols-[15%_65%] text-sm w-full gap-4 mt-4">
+        <div className="grid grid-cols-4 text-sm gap-4 mt-4">
             {type &&
                 <div className="w-12 aspect-square outline outline-2 outline-offset-2 rounded-full p-2">
                     <Icon name={type} className="w-full aspect-square"/>
                 </div>}
-            <div className="flex space-x-8 w-full ml-10">
-                {categories.map(category => <ImpactCause key={category.id} impactCategory={category}/>)}
-            </div>
-            {showIconsBelow && <div className="flex justify-center">
+            {filledUpCategories.map(category => <ImpactCause key={category.id} impactCategory={category}/>)}
+            {!type && <div></div>}
+            {showIconsBelow && <div className="flex">
                 <Icon name="info" className="h-6"/>
             </div>}
-            {showIconsBelow ?
-                <div className="space-y-2">
-                    <h1 className="text-lg">How does it help?</h1>
-                    <div className="flex space-x-12">
-                        {categories.map(category => (category.tooltips &&
-                            category.tooltips.map(tooltip => <div className="relative group">
-                                <Icon name={tooltip.icon}
-                                      className="h-9 mt-3"/>
-                                <LearnMoreTooltip tooltip={tooltip}/>
-                            </div>)))}
+            {showIconsBelow && <h1 className="text-lg col-span-3">How does it help?</h1>}
+            {showIconsBelow && tooltips.map((tooltip, index) =>
+                <>
+                    {index % 3 === 0 && <div></div>}
+                    <div className="relative group">
+                            <Icon name={tooltip.icon}
+                                  className="h-9 mt-3"/>
+                            <LearnMoreTooltip tooltip={tooltip}/>
                     </div>
-                </div> :
-                <>{!dontShowTotal && <h1 className="text-blue col-span-2 text-lg mb-4">Your impact score: 7</h1>}</>}
+                </>)}
+            {!showIconsBelow && !dontShowTotal && <h1 className="text-blue col-span-2 text-lg mb-4">Your impact score: 7</h1>}
         </div>
     );
 }
